c985232b7eb1cab9458e8ad063930f9c8878e28c
maksi <maksi@MT-ASUS-ZenBook.home>
2021-11-24 13:44:16 +0100

    ghd

--- src/MaFenetre.java
+++ src/MaFenetre.java
@@ -8,6 +8,7 @@ import java.awt.Image;
 		public static void main (String args[]) {
 			new MaFenetre(); 
 			}
+		
 		private static final long serialVersionUID = 1L;
 		private JPanel menu_pan, pan_creer_client, pan_creer_med, pan_afficher_client, pan_remplir_stock, pan_traiter_achat;
 		private JButton bouton_titre, bouton_creer_client, bouton_creer_med, bouton_affiche_client, bouton_remplir_stock, bouton_traiter_achat,
